931cf8574af8830bb37a262e681370d2d5883696
Nastasiahyk <158265899+Nastasiahyk@users.noreply.github.com>
2024-01-31 14:16:38 +0300

    Add files via upload

---
+++ PR-1.py
@@ -0,0 +1,14 @@
+#Задание1
+tseloe = 10
+drobnoe  = 8.4
+stroka = "No"
+
+big_netseloe = tseloe * 3.5
+drobnoe -= 1 
+
+tseloe / drobnoe
+big_netseloe / drobnoe
+
+stroka = stroka * 2 + "_" + "Yes" * 3
+
+print(tseloe, drobnoe, stroka, big_netseloe)
